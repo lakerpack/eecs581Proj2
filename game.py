d27a63bfc4453cd57dcdd6c392a053de1d479fd9
shaddev <shadahmed2015@gmail.com>
2024-09-12 10:54:59 -0500

    fix cast bug

--- game.py
+++ game.py
@@ -11,6 +11,7 @@ class Game:
         num_ships_per_player=input("[Player 1 & 2] Enter number of ships per player (1-5): ")
         while not(num_ships_per_player.isdigit()) or ((1<=int(num_ships_per_player)<=5) == False):
             num_ships_per_player=input("[Player 1 & 2] That is not valid. Enter number of ships per player (1-5): ")
+        num_ships_per_player = int(num_ships_per_player)
 
         self.player1.place_ships(num_ships_per_player)
         self.player2.place_ships(num_ships_per_player)
