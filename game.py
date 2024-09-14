a4f399b2bfeb3196de8ad44c7c986b1431ce8d6a
Gregory <unniat03@gmail.com>
2024-09-14 14:45:59 -0500

    block comments for game.py

--- game.py
+++ game.py
@@ -3,10 +3,18 @@ from util import is_valid_tile
 
 class Game:
     def __init__(self):
+        '''This function intializes the game class by creating two players each assigned a player number i.e., 1 & 2. '''
         self.player1 = Player(1)
         self.player2 = Player(2)
 
     def _game_setup(self):
+        '''
+        This function wil setup the game by asking each players to choose the number of ships they want to play with. 
+        The first prompt will be for the input of number of ships per player. 
+        It also ensures that the user inputs a valid value which is between 1 and 5. 
+        If the user enters an value out of the range, it will ask the user to input a valid input.
+        Also calls the place_ships method where both players are asked to place their ships on the board. 
+        '''
         num_ships_per_player = input("[Player 1 & 2 | Game Setup] Enter number of ships per player (1-5): ")
         while not num_ships_per_player.isdigit() or not (1 <= int(num_ships_per_player) <= 5):
             num_ships_per_player = input("[Player 1 & 2 | Game Setup] That is not valid. Enter number of ships per player (1-5): ")
@@ -34,6 +42,11 @@ class Game:
         return tile
     '''
     def _get_input_tile(self):
+        ''' 
+        This function will ask the current player to input a position where they would like to attack next
+        It loops until a valid position is inputed by the player. 
+        By using is_valid_tile function, it ensures that the tile format is correct.
+        '''
         while True:
             tile = input("Tile to attack (e.g., A1): ").strip().upper()
             if is_valid_tile(tile):
@@ -42,6 +55,11 @@ class Game:
                 print("Invalid input. Please enter a valid tile to attack (e.g., A1 to J10)")
     
     def run(self):
+         ''' 
+        This function will run the game loop.
+        It starts by setting up the game and placing the ships for both players in alternate turns. 
+        For each turn, the current player will attemp to hit a tile of the opponent's board. 
+        The game will come to a stop when one of the player's ships are completely destroyed by the opponent.'''
         self._game_setup()
         cur_player = self.player1
         next_player = self.player2
@@ -71,4 +89,5 @@ class Game:
         print(f"Game Over! Player {winner.player_num} has won!\n")
         
     def game_stats():
+         '''This function is a placeholder for future implementation of the game statistics which could include total turns, hit rate, accuracy etc. '''
         pass
